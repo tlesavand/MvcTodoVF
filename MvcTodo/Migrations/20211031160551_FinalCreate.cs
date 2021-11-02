063ef59588564c56c0b3672ff35984c10ef014f8
tlesavand <tlesavand@ensc.fr>
2021-11-02 18:26:14 +0100

    migration

---
+++ MvcTodo/Migrations/20211031160551_FinalCreate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcTodo.Migrations
+{
+    public partial class FinalCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Deadline",
+                table: "Todo",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deadline",
+                table: "Todo");
+        }
+    }
+}
